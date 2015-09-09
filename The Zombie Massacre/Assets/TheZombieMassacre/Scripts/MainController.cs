ea1a0204cde703c88bb59281e2579872ab44a66c
Alvin Lee <nsmae0127@gmail.com>
2015-09-09 15:04:50 +0900

    Create a main menu

---
+++ The Zombie Massacre/Assets/TheZombieMassacre/Scripts/MainController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void PlayGame (string level)
+	{
+		Application.LoadLevel (level);
+	}
+
+	public void QuitGame ()
+	{
+		Application.Quit ();
+	}
+}
